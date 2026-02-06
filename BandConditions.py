c803b22991a039240e9c1d4b507e6fb3deaea588
Kevin MacDonald <Kevin@geekfrog.ca>
2026-02-06 15:49:28 -0800

    Comment out VANCOUVER_LAT and example usage
    Commented out the VANCOUVER_LAT variable and example usage of calculate_band_conditions function.

--- BandConditions.py
+++ BandConditions.py
@@ -112,3 +112,13 @@ def calculate_band_conditions(
             conditions[band] = 'POOR'
 
     return conditions
+
+
+#VANCOUVER_LAT = 49.2827
+
+#conditions = calculate_band_conditions(
+#    solar_flux=178,
+#    k_index=4,
+#    utc_hour=23,
+#    latitude_deg=VANCOUVER_LAT
+#)
